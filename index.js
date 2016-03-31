02f7c49219b744128dffb633a21898f3ca146c5b
Dylan Piercey <pierceydylan@gmail.com>
2016-03-30 21:20:26 -0600

    Use nodes native connect normalizer

--- index.js
+++ index.js
@@ -1,27 +1,9 @@
+var normalizeConnectArgs = require('net')._normalizeConnectArgs
 var Connection = require('./lib/connection')
 
 module.exports = {
   createProxy: require('./lib/proxy'),
-  connect: function (a, b, c) {
-    // Parse connection arguments like net.connect.
-    var opts = (
-      typeof a === 'string' ? { path: a }
-      : typeof a === 'number' ? { port: a, host: b }
-      : a
-    )
-
-    if (typeof opts !== 'object') {
-      throw new TypeError('Toll: Invalid connection options.')
-    }
-
-    // Find the first object for the heatbeat option.
-    opts.heartbeat = (
-      typeof a === 'object' ? a.heartbeat
-      : typeof b === 'object' ? b.heartbeat
-      : typeof c === 'object' ? c.heartbeat
-      : undefined
-    )
-
-    return new Connection(opts)
+  connect: function () {
+    return new Connection(normalizeConnectArgs(arguments)[0])
   }
 }
